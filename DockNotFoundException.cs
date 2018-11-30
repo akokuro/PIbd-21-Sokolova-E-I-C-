65171b5e4dea431efe4a60d1a6b3403bb6f24bcc
Kurai <akokuro@mail.ru>
2018-11-30 14:44:54 +0400

    Laba7: Exception and Logger

---
+++ DockNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsCars
+{
+    /// <summary>
+    /// Класс-ошибка "Если не найден корабль по определенному месту"
+    /// </summary>
+    class DockNotFoundException : Exception
+    {
+        public DockNotFoundException(int i) : base("Не найден корабль по месту " + i)
+        { }
+    }
+}
